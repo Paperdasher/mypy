4461502b6a7a348ad8d4ef0e665817e15ed8af51
Sean Takahashi <seanyuto@gmail.com>
2024-11-29 14:16:51 -0500

    Day1 Material Temp

---
+++ Module1_IntroToCS/Day1.py
@@ -0,0 +1,59 @@
+##Day1: Intro and Data Types
+## W3Schools Explanation: https://www.w3schools.com/python/python_variables.asp
+
+## As you can notice, this is a comment and will not run when called
+## Anything after the # symbol is considered a comment for that line
+""""
+This is a multi-line comment. This can be done using three quotes to start and to end. 
+You can find more info about comments at this link: https://www.w3schools.com/python/python_comments.asp
+""""
+
+"""
+In Python, there are multiple data types. The most common ones are:
+- str(String)
+- int(Integer)
+- float(Decimal)
+- bool(Boolean)
+- list
+- dict(Dictionary)
+- range
+"""
+
+
+
+## Strings
+"""
+A String is a type where any character can be written inside a "" or a ''. It's like a conversation, anything can be inside of it.
+We can have a string of numbers "12345" or a word "Hello User!" or a combination of both "Hello User 12345!"
+Those are some of the glimpse of this data type. We can print literal strings using print(). 
+
+Multi-line strings can be written just like a comment. These comments are technically multi-line strings, but since they aren't printed
+won't affect the code.
+"""
+
+
+a = "Hello"
+print("Hola")
+print('Bonjour')
+print(a)
+
+## int
+"""
+An int is like the integers you deal with in math class. They don't contain decimals, and can be both negative or positive.
+So, 5 is a valid int and so is -2000. 
+"""
+
+b = 5
+c = -2000
+print(b)
+print(c)
+
+
+## float
+
+
+
+
+## Var
+
+## Print Statements
