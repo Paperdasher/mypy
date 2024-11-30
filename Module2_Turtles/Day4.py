ad8c1f740c519950938aec93dbcff4d74d33246e
Sean Takahashi <seanyuto@gmail.com>
2024-11-29 23:59:22 -0500

    wrote some lesson 4 material

---
+++ Module2_Turtles/Day4.py
@@ -0,0 +1,13 @@
+##Day 4: Turtles Part 2
+
+"""
+We can use the methods in the library to create different shapes.
+For instance, we can create circles by giving the radius to the circle method in the turtle library.
+
+We can also color the inside of shapes, a useful feature don't you think?
+
+When we move the turtle, we don't always want it to be drawing. Unlike drawing exercises where the artist doesn't pick up their pencil,
+we don't want continuity for all our shapes. So we need a way to determine when the turtle is drawing and when not to draw.
+We can use the penup/pen down functionality in the library to accomplish this.
+"""
+
