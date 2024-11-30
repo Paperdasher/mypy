6c03235df2c9fc5717054e531601930e36a1d584
Sean Takahashi <seanyuto@gmail.com>
2024-11-30 01:04:39 -0500

    Partial Day15 lesson

--- Module7_Loops/Day15.py
+++ Module7_Loops/Day15.py
@@ -1,1 +1,34 @@
-##Day 15: Intro to Loops
+##Day 15: Intro to Loops(while loops)
+
+"""
+When we want to perform an operation until some condition turns false, we don't want to write code over and over again.
+If we don't know how many repititions we'd need, it's impossible to write out all that code.
+We use loops to cover that need. We keep iterating until the condition is invalid, and we exit the code block.
+"""
+
+a = 1
+while(a < 10):
+    print(a) # what do you think this prints? Up to what number?
+    a += 1 
+
+print("Final value of a:", a) # what will this print?
+
+"""
+Exercises:
+
+Write a function where...
+1. Removes all odd numbers from a list
+2. Takes the sum of all elements in the list until a neg num
+3. Finds the largest number in a list
+
+"""
+
+### CODE BELOW ###
+
+#def remove_odd(nums):
+
+
+#def sumlist(nums):
+
+
+#def largestnum(nums):
