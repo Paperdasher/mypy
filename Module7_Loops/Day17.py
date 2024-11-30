ca258630bea564b321db084dd1037ed3d6153378
Sean Takahashi <seanyuto@gmail.com>
2024-11-29 22:27:57 -0500

    Complete Day17 Material(project)

---
+++ Module7_Loops/Day17.py
@@ -0,0 +1,14 @@
+##Day 17: Project 4- Creating an NGon
+
+"""
+Now that we have gone over loops, we can create a polygon of any amount of sides.
+We can also take a user input of what kind of polygon they'd like to make, and program the function so it uses that number to draw the polygon.
+
+Your task is to write a function ngon that takes a user input of a float num, and draws the corresponding sided polygon.
+Advanced: Have code that prints an error message if the input is not a int or float value(ex. string word).
+"""
+
+### CODE BELOW ###
+# As always, write the function definition above the function as a comment.
+
+def ngon():
