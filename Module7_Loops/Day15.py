b3e181b25af159c5a896f28745baf72d6fe26141
Sean Takahashi <seanyuto@gmail.com>
2024-11-30 01:20:29 -0500

    Final ver of Day15

--- Module7_Loops/Day15.py
+++ Module7_Loops/Day15.py
@@ -13,6 +13,22 @@ while(a < 10):
 
 print("Final value of a:", a) # what will this print?
 
+
+"""
+We can also use the random module to create random numbers.
+This can be useful to make a list of random numbers to test functions such as sorting algorithms.
+"""
+
+import random
+
+b = 5
+nums = []
+while(b > 1): # can write while loops to decend
+    nums.append(random.randint(0, 5)) # adds random int from 0 to 5 into nums list
+    b -= 1 
+
+print(nums) # how many values will be in the list?
+
 """
 Exercises:
 
@@ -32,3 +48,10 @@ Write a function where...
 
 
 #def largestnum(nums):
+
+
+"""
+HW: 
+1. Create a while loop that asks for a number, and either adds 1 or subtracts 1 until the number is 100.
+2. Create a factorial function.
+"""
