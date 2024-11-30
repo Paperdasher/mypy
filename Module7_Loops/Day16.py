46fc477f783a1965fdc21c1735cdc8c7bca5afe3
Sean Takahashi <seanyuto@gmail.com>
2024-11-30 00:06:19 -0500

    added Day16 file

---
+++ Module7_Loops/Day16.py
@@ -0,0 +1,15 @@
+##Day 16: For Loops
+
+"""
+Last time, we learned what a while loop is. While loops are basic loops that help us repeat actions as long as the boolean statement is True.
+Although while loops are an easy implementation of loops, they can be less functional in some cases.
+For loops can be an easier method to iterate through data types.
+"""
+
+
+def foo(a):
+    for x in a:
+        print(x)
+    
+foo(["a", "b", "c", "d"])
+
