34cae630f61c5c3c1421142d6800ee8348e48f3f
Sean Takahashi <seanyuto@gmail.com>
2024-11-30 00:17:36 -0500

    final day20 material

--- Module8_FinalProject/Day20.py
+++ Module8_FinalProject/Day20.py
@@ -1,1 +1,15 @@
 ##Day 20: Final Project
+
+"""
+Now that you've accumulated adequate coding skills using Python, put it all to test in this project.
+
+Create a 4*4 Tic Tac Toe game, which the computer randomly selects a place to mark after every user move. The player chooses what symbol to represent them(ex. x, o).
+The board should be updated after every turn and shown to the user during the plays. If the computer already chose the spot they input, make them choose another.
+Don't use turtles, simply print lines of the board.
+
+You must use helper functions for this project. Use clear variable names, and make clear what function does what.
+Ex. board_position instead of b, def computer_move instead of def comp
+"""
+
+### CODE BELOW ###
+
