e6ced47426ae38ea4ccc1a15f1585f77dd31f58d
Sean Takahashi <seanyuto@gmail.com>
2024-11-29 23:19:49 -0500

    Day6 material complete

---
+++ Module2_Turtles/Day6.py
@@ -0,0 +1,23 @@
+##Day 6: Turtles Part 3- Using Functions
+
+"""
+Knowing how to write functions, we can apply it to turtles.
+If we wanted to make a equilateral triangle of side length 10, or a square with side length 20, how would we code that?
+
+Functions allow us to give a input of a certain parameter, and the function will do the action for us without us having to manually code each case.
+This allows for easier automation, and flexibility to call the function with each necessary input. 
+
+For this exercise, create a function for drawing a triangle and a square. 
+"""
+
+### CODE BELOW ###
+
+
+## Triangle Function
+
+def triangle():
+
+
+## Square Function
+
+def square():
