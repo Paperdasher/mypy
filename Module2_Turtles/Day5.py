ae3e21d2f1b89ea8504790575f1a6760588c20b9
Sean Takahashi <seanyuto@gmail.com>
2024-11-29 21:54:58 -0500

    Complete Day5 material

---
+++ Module2_Turtles/Day5.py
@@ -0,0 +1,12 @@
+##Day 5: Project 1- Turtles
+
+"""
+For this project, use what you have learned to create/write one of the following:
+- Olympic Ring
+- Own name
+
+Remember, you are using the turtles library. Include corresponding import statements, and make sure it is a readable size.
+If you choose to do the Olympic Ring, make sure the design(colors, alignment) are the same as the official ring.
+"""
+
+### CODE BELOW ###
