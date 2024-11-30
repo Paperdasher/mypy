c826ed0d0cc21e66e570521249667097a25d8000
Sean Takahashi <seanyuto@gmail.com>
2024-11-29 21:54:30 -0500

    Made HW file

---
+++ Module2_Turtles/Day3HW.py
@@ -0,0 +1,8 @@
+##Day 3 HW- Draw a regular hexagon in blue. 
+
+"""
+Note- Regular polygons mean all sides and angles are equivalent. 
+      This means that the angles should be 120 degrees, and each side should be in equal length(any as long as they are visible).
+"""
+
+### CODE BELOW ### 
