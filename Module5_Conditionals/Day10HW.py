771f72e7b7bb9a9758411864ec582baadb53980f
Sean Takahashi <seanyuto@gmail.com>
2024-12-02 01:11:12 -0500

    added Day10 and Day11 HW files

---
+++ Module5_Conditionals/Day10HW.py
@@ -0,0 +1,11 @@
+## Day 10 HW
+
+"""
+Create a function that takes in a favorite food as a parameter.
+- if the food is equal to cake, print out "I like cake too"
+- if the food is equal to salad, print out "I don't like salad"
+- if the food is neither, print out "Sounds good"
+"""
+
+# determine what else needs to be added, including parameters
+def favfood():
---
+++ Module5_Conditionals/Day11HW.py
@@ -0,0 +1,10 @@
+## Day 11 HW
+
+"""
+Create a function that takes in the temperature as a parameter.
+- Make ranges for what constitutes "hot", "cold", "freezing", etc.
+- Print out "the temperature is cold", "... is hot", etc. depending on the range category the temp is in
+"""
+
+# determine what else needs to be added, including parameters
+def temprange():
