a6cfd39a25d09da18947d04a80d9c7831c6f9a7f
Sean Takahashi <seanyuto@gmail.com>
2024-12-02 00:44:06 -0500

    added two new material files, balanced out material to be taught, skeleton written out

--- Module6_DataStructures/Day12.py
+++ Module6_DataStructures/Day12.py
@@ -1,8 +1,26 @@
-##Day 12: Intro to Data Structures
+## Day 12: Intro to Data Structures- Lists
 
 """
+Lesson: Explain Lists
+Create a list of favorite foods and print out the 3 element
+
+
 List
 list.append(value)
 list.insert(index, value)
 list.remove()
 """
+
+# ----------------------------------------- #
+
+"""
+Exercises:
+
+1. 
+2. 
+3. 
+"""
+
+# ----------------------------------------- #
+
+## HW: Complete three CodingBat exercises in category "Lists"
