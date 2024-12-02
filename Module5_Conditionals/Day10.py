4adcc849e45e2bdd31a4559319fb6b1e639b0d29
Sean Takahashi <seanyuto@gmail.com>
2024-12-02 00:28:29 -0500

    wrote some exercises and put in homework

--- Module5_Conditionals/Day10.py
+++ Module5_Conditionals/Day10.py
@@ -1,1 +1,23 @@
 ##Day 10: Intro to Conditionals
+
+"""
+
+"""
+
+# ----------------------------------------------------- #
+
+
+"""
+Exercise:
+
+1. Make a function that takes in a number and if it is not greater than 10, add 2 to the number. If the number is still not greater than 10, return false. Otherwise true.
+2. If a string contains "a" return "I like that." If not, return "Aw man." -> Hint: _ in _ checks if a string is in another string
+"""
+
+"""
+HW: 
+Create a function that takes in a favorite food as a parameter.
+- if the food is equal to cake, print out "I like cake too"
+- if the food is equal to salad, print out "I don't like salad"
+- if the food is neither, print out "Sounds good"
+"""
