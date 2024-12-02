814a44aa23ebabac9b2463a250b4a95211dfca50
Sean Takahashi <seanyuto@gmail.com>
2024-12-01 23:57:46 -0500

    added first 7 triangle exercise, added clarity to make both equilateral and right tri funcs

--- Module2_Turtles/Day6.py
+++ Module2_Turtles/Day6.py
@@ -1,5 +1,22 @@
 ##Day 6: Turtles Part 3- Using Functions
 
+import turtle
+
+"""
+Since we know how to draw shapes, we can expand it to any type.
+Try drawing 7 triangles of different sizes, angle measurments, and orientation.
+"""
+
+### CODE BELOW ###
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+
 """
 Knowing how to write functions, we can apply it to turtles.
 If we wanted to make a equilateral triangle of side length 10, or a square with side length 20, how would we code that?
@@ -7,15 +24,20 @@ If we wanted to make a equilateral triangle of side length 10, or a square with 
 Functions allow us to give a input of a certain parameter, and the function will do the action for us without us having to manually code each case.
 This allows for easier automation, and flexibility to call the function with each necessary input. 
 
-For this exercise, create a function for drawing a triangle and a square. 
+For this exercise, create a function for drawing a equilateral triangle, right triangle, and a square. 
 """
 
 ### CODE BELOW ###
 
 
-## Triangle Function
+## Equilateral Triangle Function
 
-def triangle():
+def equitriangle():
+
+
+## Right Triangle Function
+
+def righttriangle():
 
 
 ## Square Function
