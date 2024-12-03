d905e8386e9e54359b401813c81c0449dc38c9bd
Sean Takahashi <seanyuto@gmail.com>
2024-12-03 14:46:13 -0500

    Added Day0 material explaining terminals

---
+++ Module1_IntroToCS/Day0.py
@@ -0,0 +1,55 @@
+## Day 0: Terminal Commands
+
+"""
+Welcome! This is the platform, repl.it, that we will be using for our lessons. 
+This is the repository(known as repl on this platform) but more commonly short as repo.
+Before we get into learning Python, we need to learn a bit about how to use this platform in order to access and run our Python files.
+
+In more user-friendly platforms such as Scratch, there is the "Run" feature that runs the given program.
+In code, we run our code through the terminal, which is a software on your computer. This emulates a hardware device that runs programs.
+In some cases, there are IDE(Integrated development environment) that allow you to directly run Python code.
+However, repl.it is an online platform that doesn't directly operate with a "Run" button. Instead, it provides us with a terminal in the platform.
+By giving terminal commands, we can replicate software that needs to be installed with this online platform.
+
+On your computer, you have what is commonly known as "files." In CS, we call these "directories."
+When you open the "file explorer," it's a more user-friendly version of what we can do with the terminal.
+The file explorer makes it easier to navigate through files. In the terminal, we see only the file names.
+When we write code, our programs can access other files(pictures such as .jpg, data such as .csv, .txt, other code, etc.)
+
+Common commands beneficial to know-
+- cd(change-directory): changes the current directory
+Ex. when you open the terminal: cd Documents/MyPy/Python/Module1_IntroToCS
+Tip: to go outside of a directory, use the .. keyword. (ex. cd ../.. -> goes to MyPy directory)
+
+- mkdir(make-directory): makes a new directory in the current directory
+Ex. make a new module: mkdir ModuleX
+
+To open your terminal, simply click the key with the Windows logo to popup the app finder. Then type "terminal." 
+Note: Other complex commands may differ based on the system(Windows vs. Mac vs. Linux)
+
+For Python, there are two common commands that are used to run Python files. There is the "py" and the "python" command.
+Depending on the system, one of the two will run your Python file. For this platform, the "python" keyword will run files.
+So when running your homework file, try cd-ing into the right directory, then using "python DayXHW.py"
+"""
+
+"""
+Exercise:
+
+Try on the terminal provided below on repl.it
+
+1. From Module2_Turtles, change directories into Module7_Loops
+2. Try running this file(be careful to come back into this directory). 
+"""
+
+
+
+
+
+
+
+
+
+
+
+print("Welcome Gabriel!")
+print("You've successfully run this file. Great job!")
