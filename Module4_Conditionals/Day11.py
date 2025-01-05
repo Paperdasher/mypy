588c9cf5f2357c661173352d6041ccb9e268ddd6
Sean Takahashi <seanyuto@gmail.com>
2025-01-05 17:09:58 -0500

    moved files

---
+++ Module4_Conditionals/Day11.py
@@ -0,0 +1,25 @@
+## Day 11: Comparisons
+
+"""
+We learned about comparison operators in the last module. Here, we're going to put it to test in using them in coding context.
+"""
+
+"""
+Lesson description:
+Explain and, or, greater than, less than with conditional statements- ask user to make a number variable and if the number is 16 or 20, print out that it is 16 or 20
+Also, print out that the number is small if it is less than ten
+"""
+
+### Exercise: Work on If-statements category problems on CodingBat. Share your screen so I can see you coding. Try to complete all problems.
+
+
+# ------------------------------------------------------------------------------- #
+
+"""
+HW:
+1. Create a function that takes in the temperature(int) as a parameter.
+- Make ranges for what constitutes "hot", "cold", "freezing", etc. (make it reasonable! 100 deg is not "cold")
+- Print out "the temperature is cold", "... is hot", etc. depending on the range category the temp is in
+
+2. Complete rest of If-statements category on CodingBat.
+"""
---
+++ Module4_Conditionals/Day11HW.py
@@ -0,0 +1,12 @@
+## Day 11 HW
+
+"""
+1. Create a function that takes in the temperature(int) as a parameter.
+- Make ranges for what constitutes "hot", "cold", "freezing", etc. (make it reasonable! 100 deg is not "cold")
+- Print out "the temperature is cold", "... is hot", etc. depending on the range category the temp is in
+
+2. Complete rest of If-statements category on CodingBat.
+"""
+
+# determine what else needs to be added, including parameters
+def temprange():
