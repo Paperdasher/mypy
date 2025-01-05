9f3ec0ed4cc19e30b01e31f9b5e3f898859cc37b
Sean Takahashi <seanyuto@gmail.com>
2025-01-05 17:10:41 -0500

    moved files

---
+++ Module5_DataStructures/Day14HW.py
@@ -0,0 +1,9 @@
+## Day 14 HW
+
+"""
+- Create a game where the user is asked 3-5 questions about their favorite things.
+- Store the inputs in a dictionary. 
+- Print out the dictionary at the end.
+"""
+
+def favthings():
