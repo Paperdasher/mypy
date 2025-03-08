1ef1d1dfe784facce79132241929dc6c2d6d92e2
Sean Takahashi <seanyuto@gmail.com>
2025-03-07 22:40:28 -0500

    added skeleton code

---
+++ lab00-intro/intro.py
@@ -0,0 +1,22 @@
+## Lab00- Intro ##
+
+# Create variables for your name, age, hometown, and favorite color.
+# Then, print a sentence that introduces you, using all of these variables.
+# Only for this assignment, commit every time you make a new variable to practice git add/commit/push.
+
+
+
+## Name ##
+
+
+## Age ##
+
+
+## Hometown ##
+
+
+## Favorite Color ##
+
+
+
+## Print ##
