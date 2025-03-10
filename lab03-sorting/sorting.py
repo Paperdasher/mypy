a4f0f9fcc6e304bc4c765364a7ceb7dd06ca4932
Sean Takahashi <seanyuto@gmail.com>
2025-03-10 00:40:42 -0400

    edited

---
+++ lab03-sorting/sorting.py
@@ -0,0 +1,28 @@
+# Lab03- Sorting 
+
+
+# In this lab, you will be writing three different sorting methods. 
+# The three algorithms are selection sort, a bubble sort, and a insertion sort.
+# Although there are not good algorithms to sort a list, they serve as a good educational purpose to understand list structure.
+
+
+# Read the README on the repo to understand each algorithm. 
+# Understanding the algorithm should also allow you to think about the time complexity.
+
+
+def bubblesort(data):
+    # Your code here #
+    return data
+
+
+
+
+def selectionsort(data):
+    # Your code here #
+    return data
+
+
+
+def insertionsort(data):
+    # Your code here #
+    return data
--- lab03-sorting/tester.py
+++ lab03-sorting/tester.py
@@ -7,6 +7,8 @@ import random
 import time
 import unittest
 
+from sorting import bubblesort, selectionsort, insertionsort
+
 class TestSortingAlgorithms(unittest.TestCase):
 
     def setUp(self):
@@ -14,13 +16,49 @@ class TestSortingAlgorithms(unittest.TestCase):
         self.sorted_data = sorted(self.data)
 
     def test_bubblesort(self):
-        self.assertEqual(sorting.bubblesort(self.data.copy()), self.sorted_data)
+        self.assertEqual(bubblesort(self.data.copy()), self.sorted_data)
 
     def test_selectionsort(self):
-        self.assertEqual(sorting.selectionsort(self.data.copy()), self.sorted_data)
+        self.assertEqual(selectionsort(self.data.copy()), self.sorted_data)
 
     def test_insertionsort(self):
-        self.assertEqual(sorting.insertionsort(self.data.copy()), self.sorted_data)
+        self.assertEqual(insertionsort(self.data.copy()), self.sorted_data)
+
+    def test_bubblesort_all_same(self):
+        data = [5] * 10
+        self.assertEqual(bubblesort(data.copy()), data)
+
+    def test_selectionsort_all_same(self):
+        data = [5] * 10
+        self.assertEqual(selectionsort(data.copy()), data)
+
+    def test_insertionsort_all_same(self):
+        data = [5] * 10
+        self.assertEqual(insertionsort(data.copy()), data)
+
+    def test_bubblesort_sorted(self):
+        data = list(range(10))
+        self.assertEqual(bubblesort(data.copy()), data)
+
+    def test_selectionsort_sorted(self):
+        data = list(range(10))
+        self.assertEqual(selectionsort(data.copy()), data)
+
+    def test_insertionsort_sorted(self):
+        data = list(range(10))
+        self.assertEqual(insertionsort(data.copy()), data)
+
+    def test_bubblesort_reverse(self):
+        data = list(range(10, 0, -1))
+        self.assertEqual(bubblesort(data.copy()), list(range(1, 11)))
+
+    def test_selectionsort_reverse(self):
+        data = list(range(10, 0, -1))
+        self.assertEqual(selectionsort(data.copy()), list(range(1, 11)))
+
+    def test_insertionsort_reverse(self):
+        data = list(range(10, 0, -1))
+        self.assertEqual(insertionsort(data.copy()), list(range(1, 11)))
 
 if __name__ == '__main__':
     unittest.main()
