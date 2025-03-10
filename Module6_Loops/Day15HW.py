0cf8db9e979f89ecd30b9a54099176d8761ed1e6
Sean Takahashi <seanyuto@gmail.com>
2025-03-10 00:13:41 -0400

    Update Day15HW.py

--- Module6_Loops/Day15HW.py
+++ Module6_Loops/Day15HW.py
@@ -2,7 +2,19 @@
 
 ## 1. Create a while loop that asks for a number, and either adds 1 or subtracts 1 until the number is 100.
 
-
+while n != 100:
+  if n < 100:
+    n += 1
+  else:
+    n -= 1
 
 
 ## 2. Create a factorial function.
+
+def factorial(n):
+  result = 1
+  i = 1
+  while i <= n:
+    result *= i
+    i += 1
+  return result
