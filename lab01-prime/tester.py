58891accc9d5a5d116e55e4619e298459ae692fa
Sean Takahashi <seanyuto@gmail.com>
2025-03-10 00:23:09 -0400

    wrote testing code

--- lab01-prime/tester.py
+++ lab01-prime/tester.py
@@ -3,12 +3,36 @@
 from prime import is_prime
 from prime import nthPrime
 
+
+def isPrimeC(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def nthPrime(n):
+    count = 0
+    i = 2
+    while count < n:
+        if isPrimeC(i):
+            count += 1
+        i += 1
+    return i - 1
+
+
 print("Testing is_prime()")
-print("is_prime(1) == False: ", is_prime(1))
-print("is_prime(2) == True: ", is_prime(2))
-print("is_prime(3) == True: ", is_prime(3))
-print("is_prime(4) == False: ", is_prime(4))
+for i in range(0, 100):
+    if isPrimeC(i) != is_prime(i):
+        print("is_prime() failed on input: ", i)
+        break
+
+
 
 print("Testing nthPrime()")
-print("nthPrime(0) == 2: ", nthPrime(0))
-print("nthPrime(1) == 3: ", nthPrime(1))
+
+for i in range(0, 100):
+    if nthPrime(i) != nthPrime(i):
+        print("nthPrime() failed on input: ", i)
+        break
