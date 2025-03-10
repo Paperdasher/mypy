45f12603e9bfa03176ff1646830115e0cb9c6c35
Sean Takahashi <seanyuto@gmail.com>
2025-03-10 00:18:31 -0400

    added files

---
+++ lab01-prime/prime.py
@@ -0,0 +1,23 @@
+## Lab01- Prime ##
+
+# You will be writing two functions, isPrime and nthPrime. 
+# Read the function documentation carefully. 
+# Test your code thoroughly.
+
+# return true if and only if x is prime, false otherwise
+# assume n is an integer
+def isPrime(n):
+    # Check divisibility for all values from 2 to n-1 inclusive
+    # Although this is not a good algorithm, use this for simplicity
+    return False
+
+
+
+
+
+# Return the nth prime
+# e.g. nthPrime(0) returns 2, nthPrime(1) returns 3
+def nthPrime(n):
+    # Loop through values and check if they are prime until you find n of them
+    # Use your isPrime function to check if a number is prime
+    return -1
---
+++ lab01-prime/tester.py
@@ -0,0 +1,14 @@
+## Lab01 TESTER ##
+
+from prime import is_prime
+from prime import nthPrime
+
+print("Testing is_prime()")
+print("is_prime(1) == False: ", is_prime(1))
+print("is_prime(2) == True: ", is_prime(2))
+print("is_prime(3) == True: ", is_prime(3))
+print("is_prime(4) == False: ", is_prime(4))
+
+print("Testing nthPrime()")
+print("nthPrime(0) == 2: ", nthPrime(0))
+print("nthPrime(1) == 3: ", nthPrime(1))
